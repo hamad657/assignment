06c6d4aa6e7d30d50740ea3bcf0afea7af2fa03d
hamad657 <143127660+hamad657@users.noreply.github.com>
2023-09-06 23:18:26 +0500

    Add files via upload

---
+++ Untitled2.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+int main ()
+{
+    //data and values
+    string name="hajra";
+    int age=12;
+    char selection='A';
+    string subject="computer science";
+    int mark=90;
+    float avg=9.60;
+    //cout statement
+    cout<<"----students details----/n";
+    cout<<"name="<<name<<"/n";
+    cout<<"age="<<age<<"/n";
+    cout<<"selectio="<<selection<<"/n";
+    cout<<"subject="<<subject<<"/n";
+    cout<<"mark="<<mark<<"/n";
+    cout<<"avg="<<avg<<"/n";
+    return 0;
+ }
+    
+    
